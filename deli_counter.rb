5c6bd3cd6e7865c7c1aa07525b2cf4150cbca9df
github username <github email address>
2019-05-20 22:26:11 -0400

    Strings are values too, don't forget.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+    if array.size > 0
+        storage_array = []
+        number = 1
+        opening_message = "The line is currently:"
+        array.each do |name|
+            storage_array.push(" #{number}. #{name}")
+            number += 1
+        end
+        storage_array.each do |numbername|
+            opening_message += "#{numbername}"
+        end
+        puts opening_message
+    else 
+        puts "The line is currently empty."
+    end        
+end
+
+def take_a_number(array, name)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
